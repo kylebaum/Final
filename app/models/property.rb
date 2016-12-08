94471bf5d7fd521ceb95a269a3317854fa223de1
Kyle Baum <username@users.noreply.github.com>
2016-12-08 16:46:38 -0600

    with more validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -6,5 +6,8 @@ class Property < ActiveRecord::Base
   has_many :maintenances
 
   validates :user_id, :presence => true
-  
+  validates :price, :numericality => { :less_than_or_equal_to => 20000, :greater_than_or_equal_to => 100 }
+  validates :numberofbedroom, :numericality => { :less_than_or_equal_to => 20, :greater_than_or_equal_to => 0 }
+  validates :numberofbathroom, :numericality => { :less_than_or_equal_to => 20, :greater_than_or_equal_to => 1 }
+  validates :address, :presence => true
 end
