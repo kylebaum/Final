737cf5f28230f92ffac45c26fe3ba643cf4e7465
Kyle Baum <username@users.noreply.github.com>
2016-12-08 15:38:39 -0600

    with user working

--- config/routes.rb
+++ config/routes.rb
@@ -103,9 +103,12 @@ Rails.application.routes.draw do
   get "/delete_property/:id", :controller => "properties", :action => "destroy"
   #------------------------------
 
-  get "/users", :controller => "users", action: "index"
-  get "/users/:id", :controller => "users", :action => "show"
+
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/users", :controller => "users", action: "index"
+  get "/users/:id", :controller => "users", :action => "show"
 end
